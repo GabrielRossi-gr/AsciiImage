2dd2499b83d6fb6c11c20e0f833b9545ad26c272
ga Rossi <gabrieldesouzarossi123456@gmail.com>
2024-09-12 10:23:30 -0300

    update

--- main.py
+++ main.py
@@ -3,16 +3,10 @@
 
 #import pywhatkit as KT
 
-#KT.image_to_ascii_art("C:/Users/gabri/Downloads/IMG_20221031_183903_824 (2).jpg","C:/Users/gabri/Downloads/IMG_20221031_183903_824 (2).txt")
-
-
-<<<<<<< HEAD
 from PIL import Image # type: ignore
 
 #palmeiras
 imagem = Image.open("/Users/gabrielrossi/Downloads/kkk1.png")
-
-
 
 
 #medio: 
@@ -20,20 +14,6 @@ largura, altura = 80 , 50
 
 #largo: largura, altura = 480 , 300
 #pequeno:largura, altura = 200 , 100 
-=======
-from PIL import Image
-
-
-
-
-#image link 
-imagem = Image.open("C:/Users/gabri/Documents/asciiArt/palmeiras img.png")
-
-#medio: 
-largura, altura = 400 , 200 
-#largo: largura, altura = 480 , 300 
-#pequeno: largura, altura = 200 , 100 
->>>>>>> ad73495ac25f1fa937d83219286b20a2a7299080
 
 imagem = imagem.resize((largura, altura))
 imagem = imagem.convert("L")
