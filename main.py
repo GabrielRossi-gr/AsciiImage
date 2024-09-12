4862db7ebf9ce0655df10eee9344162b76ae8b98
ga Rossi <gabrieldesouzarossi123456@gmail.com>
2024-09-12 10:15:57 -0300

    update

--- main.py
+++ main.py
@@ -6,31 +6,25 @@
 #KT.image_to_ascii_art("C:/Users/gabri/Downloads/IMG_20221031_183903_824 (2).jpg","C:/Users/gabri/Downloads/IMG_20221031_183903_824 (2).txt")
 
 
-from PIL import Image
-
-# vó imagem = Image.open("C:/Users/gabri/Documents/asciiArt/IMG_20220806_093347_622 (1).jpg")
-
-
-#mae   imagem = Image.open()
-
-#pai:  imagem = Image.open("C:/Users/gabri/Documents/asciiArt/IMG_20230402_154332_320.jpg")
-
-#eu: imagem = Image.open("C:/Users/gabri/Documents/asciiArt/IMG_20230402_155111_737.jpg")
-
+from PIL import Image # type: ignore
 
 #palmeiras
-imagem = Image.open("C:/Users/gabri/Documents/asciiArt/palmeiras img.png")
+imagem = Image.open("/Users/gabrielrossi/Downloads/kkk1.png")
+
+
+
 
 #medio: 
-largura, altura = 400 , 200 
-#largo: largura, altura = 480 , 300 
+largura, altura = 80 , 50 
+
+#largo: largura, altura = 480 , 300
 #pequeno:largura, altura = 200 , 100 
 
 imagem = imagem.resize((largura, altura))
 imagem = imagem.convert("L")
 
-#caracteres_ascii = [" ", ".", "-", "*", ":", "=", "+", "#", "%", "@"]
-caracteres_ascii = ["°", "/", "*", ";", "=", "+", "#", "%", "@"]         ##    !@#$%¨&*(_+=´
+caracteres_ascii = [" ", ".", "-", "*", ":", "=", "+", "#", "%", "@"]
+# caracteres_ascii = ["°", "/", "*", ";", "=", "+", "#", "%", "@"]         ##    !@#$%¨&*(_+=´
 
 pixels = imagem.load()
 ascii_image = ""
